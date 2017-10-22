62f087b89c0447a6784c906213e372a1204a3a5a
asllan <asllan77@gmail.com>
2017-10-22 09:33:17 -0400

    get contacts data using REST API and promise

--- contact-app/contact.controller.js
+++ contact-app/contact.controller.js
@@ -1,9 +1,14 @@
 angular.module("contactApp")
     .controller("ContactCtrl", function (ContactDataSvc) {
 
-    this.contacts = ContactDataSvc.contacts;
+        var self = this;
 
-    this.selectContact = function (index) {
-        this.selectedContact = this.contacts[index];
-    };
-});
+        ContactDataSvc.getContacts()
+            .then(function (data) {
+                self.contacts = data;
+            });
+
+        this.selectContact = function (index) {
+            this.selectedContact = this.contacts[index];
+        };
+    });
